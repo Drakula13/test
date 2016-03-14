bd70232fa998579f09ef074be8229f642a9451f0
Кочанов Дмитрий <C:\Users\kochanov\AppData\Roaming\The Bat!>
2016-03-14 15:14:01 +0300

    test

--- server10.py
+++ server10.py
@@ -1,4 +1,4 @@
-import asyncore, socket, collections
+import asyncore, socket
 class async_http(asyncore.dispatcher):
 
 	def __init__(self, port):
@@ -12,22 +12,13 @@ class async_http(asyncore.dispatcher):
 		return async_http_handler(client)
 		
 class async_http_handler(asyncore.dispatcher):
-
-	def __init__(self, sock = None):
-		asyncore.dispatcher.__init__(self,sock)
-		self.got_request = False
-		self.request_data = b""
-		self.write_queue = collections.deque()
-		self.responding = False
-		
-	def readable(self):
-		return not self.got_request
 		
 	def handle_read(self):
-		chunk = self.recv(1024)
-		self.request_data += chunk
-		if self == "close" | self == "Close": 
+		data = self.recv(1024)
+		if data == "close": 
 			self.close()
-		
+		else: 
+			self.send(data)
+			
 a = async_http(2222)
 asyncore.loop()
