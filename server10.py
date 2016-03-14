5f80c4ee87f8ed13b20e9322008fa6f843be507d
Кочанов Дмитрий <C:\Users\kochanov\AppData\Roaming\The Bat!>
2016-03-14 13:03:04 +0300

    test

---
+++ server10.py
@@ -0,0 +1,24 @@
+import socket
+class http_client(asyncore.dispatcher):
+
+	def __init__(self):
+		self = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.bind(('0.0.0.0', 2222))
+		self.listen(11)
+		
+	def handle_connect(self):
+		pass
+			
+	def handle_close(self):
+		self.close()
+			
+	def handle_read(self):
+		self.recv(1024)
+	
+	def writable(self):
+		return (len(self.buffer) > 0)
+			
+	def handle_write(self):
+		sent = self.send(self.buffer)
+		self.buffer = self.buffer[sent:]		
+asyncore.loop()
